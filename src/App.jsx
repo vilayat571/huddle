fae01ef8f8da6b1b80ce3e742e311e7161f373e7
vilayat571 <vilayat571@.com>
2022-09-29 22:57:53 -0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./components/styled/Container.styled";
+
+function App() {
+  return (
+    <Container>
+      <h1>App</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/styled/Container.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 1000px;
+  padding: 0 20px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
